f60a5c7474110d89bcb0aa7673cf27654267c79a
antoniobu74 <antoniobuquera@gmail.com>
2021-07-23 20:03:05 -0300

    Alteração do Curriculo em JS

--- Codai/CSS/index.js
+++ Codai/CSS/index.js
@@ -1,7 +1,34 @@
-var nome = "Antonio Neto";
+var nome = "Antonio Buquera Neto";
+var cargo = "Developer"
 
-function dizerOla(){
-    alert("Olá Antonio Neto");
+var nomeHtml = document.getElementById("nome-no-html");
+var cargoHtml = document.getElementById("cargo-no-html");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+function colocarNomeNoHtml(nome) {
+    nomeHtml.innerHTML = nome;
 }
 
+function ColocarCargoNoHtml(cargo) {
+    cargoHtml.innerHTML = cargo;
+}
 
+function logarNome(){
+    console.log(nome);
+}
+
+function clickNoProjetos(){
+    console.log("Clicou no botão projetos");
+    texto2.style.display = "block";
+    texto1.style.display = "none";
+}
+
+function clickNoSobre(){
+    console.log("Clicou no botão sobre");
+    texto1.style.display = "block";
+    texto2.style.display = "none";
+}
+
+colocarNomeNoHtml(nome);
+ColocarCargoNoHtml(cargo);
